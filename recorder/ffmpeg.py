921add8cd11cbd370d7142f594895a9739975bf3
Jim Liu <hldh214@gmail.com>
2019-08-30 19:30:32 +0800

    feat(ffmpeg): add duration && valid

--- recorder/ffmpeg.py
+++ recorder/ffmpeg.py
@@ -1,3 +1,4 @@
+import json
 import subprocess
 
 
@@ -8,6 +9,32 @@ def record(input_url, output_file):
 
     for line in ff.stdout:
         print(line.decode(), end='')
+
+
+def duration(input_file):
+    """
+    :param input_file: video's path
+    :return: video's duration, False if video is not valid
+    """
+    proc = subprocess.run([
+        'ffprobe', '-print_format', 'json',
+        '-show_format', '-i', input_file
+    ], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+
+    res = json.loads(proc.stdout)
+
+    if not res:
+        return False
+
+    return int(float(res['format']['duration']))
+
+
+def valid(input_file):
+    return bool(duration(input_file))
+
+
+def split(input_file, chunk=10 * 3600):
+    pass
 
 
 if __name__ == '__main__':
