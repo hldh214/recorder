1eac44920483cb95055a50e3f8e098d305736132
Jim Liu <hldh214@gmail.com>
2019-09-10 14:44:11 +0800

    chore(setup.py): requirements.txt welcome back

--- setup.py
+++ setup.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-requires = [
-    'requests==2.22.*',
-    'google-api-python-client==1.7.*',
-    'google-auth-oauthlib==0.4.*',
-    'google-auth-httplib2==0.0.3',
-    'toml==0.10.*',
-    'httplib2==0.13.*',
-    'tqdm==4.35.*'
-]
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 with open('README.md') as f:
     readme = f.read()
@@ -28,5 +21,5 @@ setup(
     license=_license,
     packages=find_packages(exclude=('tests', 'docs')),
     python_requires='~=3.5',
-    install_requires=requires
+    install_requires=requirements
 )
