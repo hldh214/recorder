e22a76f34958d3e1a30f454c9110495043f542be
Jim Liu <hldh214@gmail.com>
2019-10-11 11:39:50 +0800

    feat(setup): bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('LICENSE') as f:
 
 setup(
     name='recorder',
-    version='1.1.1',
+    version='1.1.2',
     description='stream recorder && automatic upload',
     long_description=readme,
     author='Jim Liu',
