9585de7176baee312d41f3ea299fb7d0177ae63a
hldh214 <hldh214@gmail.com>
2020-07-29 23:09:40 +0800

    feat(ffmpeg): update USER_AGENT

--- recorder/ffmpeg.py
+++ recorder/ffmpeg.py
@@ -4,8 +4,7 @@ import subprocess
 
 TIMEOUT_US = str(60 * 1000000)
 MAX_DURATION = str(10 * 3600 - 300)
-USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 ' \
-             'TaoBrowser/2.0 Safari/536.11'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11'
 FFMPEG_BINARY = 'ffmpeg'
 FFPROBE_BINARY = 'ffprobe'
 
