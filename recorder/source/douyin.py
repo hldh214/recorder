c69e9ebdec7563ae77464d777703c235c75fccae
hldh214 <hldh214@gmail.com>
2023-01-05 18:07:41 +0900

    feat(source): add douyin.py
    Signed-off-by: hldh214 <hldh214@gmail.com>

---
+++ recorder/source/douyin.py
@@ -0,0 +1,38 @@
+import json
+import re
+import urllib.parse
+
+import requests
+
+render_data_pattern = re.compile(r'<script\s+id="RENDER_DATA"\s+type="application/json">(.+?)</script>')
+
+
+def get_stream(room_id):
+    res = requests.get(f'https://live.douyin.com/{room_id}', headers={
+        'cookie': '__ac_nonce=063b59ce0001243a9217f;',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/108.0.0.0 Safari/537.36'
+    })
+
+    render_data_result = render_data_pattern.findall(res.text)
+
+    if not render_data_result:
+        return False
+
+    render_data = json.loads(urllib.parse.unquote(render_data_result[0]))
+
+    room_data = render_data['app']['initialState']['roomStore']['roomInfo']['room']
+
+    if 'stream_url' not in room_data:
+        return False
+
+    return room_data['stream_url']['flv_pull_url']['FULL_HD1']
+
+
+if __name__ == '__main__':
+    import time
+
+    while True:
+        print(get_stream(590890573))
+        print(get_stream(73586090754))
+        time.sleep(5)
