c7014cb78efc7e060beb2ed45170fcc6a2fefc3c
hldh214 <hldh214@gmail.com>
2023-01-18 11:48:03 +0900

    feat(douyin): add collector.py proto
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- recorder/danmaku/douyin/collector.py
+++ recorder/danmaku/douyin/collector.py
@@ -1,7 +1,8 @@
+import logging
 import time
 
 import click
-from playwright.sync_api import sync_playwright
+import playwright.sync_api
 
 from recorder.source import douyin
 
@@ -21,12 +22,34 @@ def sub(room_id, interval):
             time.sleep(interval)
             continue
 
-        with sync_playwright() as p:
-            browser = p.chromium.launch()
+        with playwright.sync_api.sync_playwright() as p:
+            browser = p.chromium.launch(headless=False)
             page = browser.new_page()
+
+            def handle_route(route: playwright.sync_api.Route):
+                # Fetch original response.
+                response = page.request.fetch(route.request)
+                # Add a prefix to the title.
+                body = response.text()
+                logging.info(body)
+                # body = body.replace("<title>", "<title>My prefix:")
+                route.fulfill(
+                    # Pass all fields from the response.
+                    response=response,
+                    # Override response body.
+                    body=body,
+                )
+
+            page.route('**/*.js', handle_route)
+            page.route("**/*.{png,jpg,jpeg}", lambda route: route.abort())
             page.goto(f'https://live.douyin.com/{room_id}')
             print(page.title())
-            browser.close()
+
+            while True:
+                time.sleep(1)
+                if not douyin.get_stream(room_id):
+                    # live ended
+                    break
 
 
 if __name__ == '__main__':
