10615a479b740833d0c25d205924e7abb9bf39cc
hldh214 <hldh214@gmail.com>
2023-02-08 16:43:56 +0900

    feat(danmaku): imp ws-server.py
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- recorder/danmaku/douyin/user-script-auto-close-dead-tab.js
+++ recorder/danmaku/douyin/user-script-auto-close-dead-tab.js
@@ -5,17 +5,21 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://live.douyin.com/*
-// @grant        none
+// @grant        window.close
 // @run-at       document-end
 // ==/UserScript==
 
 (function () {
   'use strict';
 
+  if (!('is_danmaku_dead' in unsafeWindow)) {
+    return;
+  }
+
   console.log('Auto close dead douyin danmaku tab running...');
 
   setInterval(() => {
-    if (window.is_danmaku_dead) {
+    if (unsafeWindow.is_danmaku_dead) {
       window.close();
     }
   }, 1000);
