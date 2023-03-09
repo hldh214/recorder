2a2368f2e09f9b00bdef13166b7ea82c678898e8
hldh214 <hldh214@gmail.com>
2023-03-09 18:09:00 +0900

    fix(thumbnail): rework using function
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- docs/thumbnail/generate.sh
+++ docs/thumbnail/generate.sh
@@ -7,20 +7,24 @@ if [[ $# -lt 1 ]]; then
     exit 1
 fi
 
-find $1 -name "*.mp4" | while read video; do
-    if lsof "$video" > /dev/null; then
-        echo "Skipping $video because it is being used by another process"
-        continue
+process () {
+    if lsof "$1" > /dev/null; then
+        echo "Skipping $1 because it is being used by another process"
+        return
     fi
 
-    echo "Processing $video"
+    echo "Processing $1"
     # pick random frames from video
-    rm -rf "$video.frames"
-    mkdir -p "$video.frames"
-    (cd ~/recorder && pipenv run python recorder/ffmpeg.py generate_candidate_thumbnails "$video" "$video.frames")
+    rm -rf "$1.frames"
+    mkdir -p "$1.frames"
+    (cd ~/recorder && pipenv run python recorder/ffmpeg.py generate_candidate_thumbnails "$1" "$1.frames")
     # get NSFW score for each frame and keep only the frame with the highest NSFW score
-    (cd ~/tensorflow-open_nsfw && python ~/tensorflow-open_nsfw/classify_nsfw.py pred_folder "$video.frames" -k)
-    # move the frame with the highest NSFW score to the video folder and rename it to $video.thumbnail.jpg
-    mv "$video".frames/*.jpg "$video".thumbnail.jpg
-    rm -rf "$video.frames"
-done
+    (cd ~/tensorflow-open_nsfw && python ~/tensorflow-open_nsfw/classify_nsfw.py pred_folder "$1.frames" -k)
+    # move the frame with the highest NSFW score to the video folder and rename it to $1.thumbnail.jpg
+    mv "$1".frames/*.jpg "$1".thumbnail.jpg
+    rm -rf "$1.frames"
+}
+
+export -f process
+
+find $1 -name "*.mp4" -type f -exec bash -c 'process "$0"' {} \;
