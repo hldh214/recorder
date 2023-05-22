650d0ed8443ff67a7dafb6cfbd22bf681059303c
hldh214 <hldh214@gmail.com>
2023-05-22 14:22:59 +0900

    feat(utils): add rclone_moveto.py
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- recorder/utils/README.md
+++ recorder/utils/README.md
@@ -7,3 +7,7 @@ Guess huya official replay url.
 ### df_check
 
 Check the disk space.
+
+### rclone_moveto
+
+Move the files to the remote server.
---
+++ recorder/utils/rclone_moveto.py
@@ -0,0 +1,38 @@
+import glob
+import logging
+import os
+import subprocess
+import time
+
+from recorder.uploader import sizeof_fmt
+from recorder import config, base_path
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+
+def main():
+    record_folder_path = os.path.join(base_path, config['app']['video_path'], 'record/')
+
+    while True:
+        files = glob.glob(str(f'{record_folder_path}/**/*.mp4'), recursive=True)
+
+        for file in files:
+            # check if recently modified
+            if os.path.getmtime(file) > time.time() - 10:
+                logging.info(f'{file} is busy, skip')
+                continue
+
+            logging.info(f'Processing {file}: {sizeof_fmt(os.path.getsize(file))}')
+            command = [
+                'rclone', 'moveto', '--progress', '--bwlimit', '700K',
+                file, f'upload/{file.replace(record_folder_path, "")}'
+            ]
+            logging.info(' '.join(command))
+            subprocess.run(command, check=True)
+            logging.info(f'Done {file}')
+
+        time.sleep(30)
+
+
+if __name__ == '__main__':
+    main()
