1e9a04b2ef344e55d7d36646ebad9befb6dcdc78
hldh214 <hldh214@gmail.com>
2023-08-16 14:37:17 +0900

    feat(rclone): add `watch_and_copy`
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- recorder/utils/rclone.py
+++ recorder/utils/rclone.py
@@ -1,4 +1,5 @@
 import glob
+import json
 import logging
 import os
 import subprocess
@@ -6,8 +7,8 @@ import time
 
 import fire
 
-from recorder.uploader import sizeof_fmt
 from recorder import config, base_path
+from recorder.utils import sizeof_fmt
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
@@ -34,6 +35,32 @@ def moveto(rname, bwlimit='off'):
             logging.info(f'Done {file}')
 
         time.sleep(30)
+
+
+def watch_and_copy(rname):
+    od_folder_path = os.path.join(base_path, config['app']['video_path'], 'od/')
+
+    existing_videos = set()
+    while True:
+        proc = subprocess.run([
+            'rclone', 'lsjson', '--recursive', f'{rname}:upload'
+        ], check=True, capture_output=True)
+        files = json.loads(proc.stdout)
+        videos = [f for f in files if f['MimeType'] == 'video/mp4']
+
+        new_videos = [f for f in videos if f not in existing_videos]
+        for each_video in new_videos:
+            logging.info(f'Processing {each_video["Path"]}: {sizeof_fmt(each_video["Size"])}')
+            command = [
+                'rclone', 'copy', '--progress',
+                f'{rname}:upload/{each_video["Path"]}', f'{od_folder_path}/{each_video["Path"]}'
+            ]
+            logging.info(' '.join(command))
+            subprocess.run(command, check=True)
+            logging.info(f'Done {each_video["Path"]}')
+
+        existing_videos.update([f for f in videos])
+        time.sleep(120)
 
 
 if __name__ == '__main__':
