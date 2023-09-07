2a8b7b0ad5ef2fde76f5c537f77cc1c26ab0234d
hldh214 <hldh214@gmail.com>
2023-09-07 11:45:53 +0900

    fix(source): update douyin `get_room_info`
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- recorder/source/douyin.py
+++ recorder/source/douyin.py
@@ -1,6 +1,4 @@
 import json
-import re
-import urllib.parse
 
 import requests
 
@@ -8,30 +6,37 @@ from recorder import logger
 
 REQUEST_TIMEOUT = 5
 
-render_data_pattern = re.compile(r'<script\s+id="RENDER_DATA"\s+type="application/json">(.+?)</script>')
-
 
 def get_room_info(room_id):
     try:
-        res = requests.get(f'https://live.douyin.com/{room_id}', headers={
-            'cookie': '__ac_nonce=063b59ce0001243a9217f;',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                          'Chrome/108.0.0.0 Safari/537.36'
-        }, timeout=REQUEST_TIMEOUT)
+        res = requests.get(
+            # Credit: https://github.com/LyzenX/DouyinLiveRecorder/commit/c83a00c8c2d4f4aad25347f3163cd9ced212c99d
+            f'https://live.douyin.com/webcast/room/web/enter/?aid=6383&device_platform=web&browser_language=zh-CN'
+            f'&browser_platform=Win32&browser_name=Chrome&browser_version=109.0.0.0&web_rid={room_id}',
+            headers={
+                # todo: make this cookie configurable
+                # Credit: https://github.com/ihmily/DouyinLiveRecorder/commit/6018b8f4650f9acd30bea3fca61a66534c9d8184
+                'cookie': 'ttwid=1%7CB1qls3GdnZhUov9o2NxOMxxYS2ff6OSvEWbv0ytbES4'
+                          '%7C1680522049%7C280d802d6d478e3e78d0c807f7c487e7ffec0ae4e5fdd6a0fe74c3c6af149511',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                              'Chrome/108.0.0.0 Safari/537.36',
+                'Referer': 'https://live.douyin.com/',
+            },
+            timeout=REQUEST_TIMEOUT
+        )
     except requests.exceptions.RequestException:
         return {}
 
-    render_data_result = render_data_pattern.findall(res.text)
-
-    if not render_data_result:
+    try:
+        res = res.json()
+    except requests.exceptions.JSONDecodeError:
+        logger.warning(f'Failed to get room info({room_id}), res: {res.text}')
         return {}
 
-    render_data = json.loads(urllib.parse.unquote(render_data_result[0]))
-
     try:
-        return render_data['app']['initialState']['roomStore']['roomInfo']['room']
+        return res['data']['data'][0]
     except KeyError:
-        logger.debug(f'Failed to get room data, render_data: {render_data}')
+        logger.warning(f'Failed to get data, res: {res.text}')
         return {}
 
 
@@ -64,6 +69,5 @@ if __name__ == '__main__':
     logger.setLevel(logging.DEBUG)
 
     while True:
-        print(get_stream(289698124199))
-        print(get_stream(73586090754))
+        print(get_stream(590890573))
         time.sleep(5)
