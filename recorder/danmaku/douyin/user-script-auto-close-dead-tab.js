8ee8cb7ad79ceec0f03243018fe2d3c581005f53
hldh214 <hldh214@gmail.com>
2023-12-04 15:47:49 +0900

    feat(danmaku): imp userscript
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- recorder/danmaku/douyin/user-script-auto-close-dead-tab.js
+++ recorder/danmaku/douyin/user-script-auto-close-dead-tab.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Auto close dead douyin danmaku tab
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.1.0
 // @description  try to take over the world!
 // @author       You
 // @match        https://live.douyin.com/*
@@ -18,9 +18,39 @@
 
   console.log('Auto close dead douyin danmaku tab running...');
 
+  let path = window.location.pathname;
+  // key: douyin-danmaku-path-timestamp
+  let storageKeyPrefix = 'douyin-danmaku-' + path;
+  let storageKey = storageKeyPrefix + '-' + Date.now();
+
+  localStorage.setItem(storageKey, 'alive');
+
   setInterval(() => {
     if (unsafeWindow.is_danmaku_dead) {
       window.close();
     }
+
+    let pageList = [];
+    for (let i = 0; i < localStorage.length; i++) {
+      let key = localStorage.key(i);
+      if (key.startsWith(storageKeyPrefix)) {
+        let ts = key.split('-').pop();
+        pageList.push({
+          key,
+          ts,
+        });
+      }
+    }
+    if (pageList.length > 1) {
+      console.log('More than one page alive, close the oldest one.')
+      console.log('pageList', pageList)
+      pageList.sort((a, b) => a.ts - b.ts);
+      let key = pageList[0].key;
+      localStorage.removeItem(key);
+      if (key === storageKey) {
+        window.close();
+      }
+    }
+
   }, 1000);
 })();
