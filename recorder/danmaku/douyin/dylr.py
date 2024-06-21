fb7e7a9d67c2c4ce8028ec6c6105961cd24884fc
hldh214 <hldh214@gmail.com>
2024-06-21 09:50:27 +0900

    Update Douyin's danmu websocket URL and headers
    The websocket URL for Douyin's danmu has been updated to reflect the changes in their streaming service. The URL now points to a different server, and some parameters in the URL have been changed including the webcast SDK version and room_id. The headers in the websocket connection have also been modified, removing the cookie and updating the user agent.
    
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- recorder/danmaku/douyin/dylr.py
+++ recorder/danmaku/douyin/dylr.py
@@ -19,18 +19,21 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 
 def get_danmu_ws_url(id_str):
-    return (f"wss://webcast3-ws-web-lf.douyin.com/webcast/im/push/v2/?app_name=douyin_web&version_code=180800"
-            f"&webcast_sdk_version=1.3.0&update_version_code=1.3.0&compress=gzip&internal_ext=internal_src:dim"
-            f"|wss_push_room_id:{id_str}|wss_push_did:7184667748424615439|dim_log_id"
+    return (f"wss://webcast5-ws-web-lf.douyin.com/webcast/im/push/v2/?app_name=douyin_web&version_code=180800"
+            f"&webcast_sdk_version=1.0.14-beta.0&update_version_code=1.0.14-beta.0"
+            f"&compress=gzip&internal_ext=internal_src:dim"
+            f"|wss_push_room_id:{id_str}|wss_push_did:7382749321496675881|dim_log_id"
             f":2023011316221327ACACF0E44A2C0E8200|fetch_time:$"
             f"{int(time.time())}123|seq:1|wss_info:0-1673598133900-0-0|wrds_kvs:WebcastRoomRankMessage"
-            f"-1673597852921055645_WebcastRoomStatsMessage-1673598128993068211&cursor=u-1_h-1_t-1672732684536_r-1_d-1"
+            f"-1673597852921055645_WebcastRoomStatsMessage-1673598128993068211&cursor=t-1718930306521_r-1_d-1_u-1_fh-1"
             f"&host=https://live.douyin.com&aid=6383&live_id=1&did_rule=3&debug=false&endpoint=live_pc&support_wrds=1"
             f"&im_path=/webcast/im/fetch/&device_platform=web&cookie_enabled=true&screen_width=1228&screen_height=691"
-            f"&browser_language=zh-CN&browser_platform=Mozilla&browser_name=Mozilla&browser_version=5.0%20("
-            f"Windows%20NT%2010.0;%20Win64;%20x64)%20AppleWebKit/537.36%20(KHTML,"
-            f"%20like%20Gecko)%20Chrome/100.0.4896.75%20Safari/537.36&browser_online=true&tz_name=Asia/Shanghai"
-            f"&identity=audience&room_id={id_str}&heartbeatDuration=0&signature=00000000")
+            f"&browser_language=zh-CN&browser_platform=Mozilla&browser_name=Mozilla"
+            f"&browser_version=5.0%20(Windows%20NT%2010.0;%20Win64;%20x64)"
+            f"%20AppleWebKit/537.36%20(KHTML,%20like%20Gecko)%20Chrome/126.0.0.0%20Safari/537.36"
+            f"&browser_online=true&tz_name=Asia/Shanghai"
+            f"&identity=audience&room_id={id_str}&heartbeatDuration=0&signature=6dL3Y5Qk0hp77FXh"
+            f"&user_unique_id=7382749321496675881")
 
 
 async def consumer_handler(websocket, room_id):
@@ -101,12 +104,8 @@ async def subscribe(room_id, interval):
 
         async with websockets.connect(
             ws_url,
-            user_agent_header='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)',
-            extra_headers={
-                # todo: make this cookie configurable
-                'cookie': 'ttwid=1%7CB1qls3GdnZhUov9o2NxOMxxYS2ff6OSvEWbv0ytbES4'
-                          '%7C1680522049%7C280d802d6d478e3e78d0c807f7c487e7ffec0ae4e5fdd6a0fe74c3c6af149511',
-            }
+            user_agent_header='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                              'Chrome/126.0.0.0 Safari/537.36',
         ) as websocket:
             logging.info(f'Connected to websocket: {room_id}')
             await consumer_handler(websocket, room_id)
