653e9cc69f568e1c11e59901f1faae038d588930
hldh214 <hldh214@gmail.com>
2025-08-19 12:32:20 +0900

    Refactor proxy handling in panda.py for improved clarity
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- recorder/source/panda.py
+++ recorder/source/panda.py
@@ -11,12 +11,9 @@ def get_stream(room_id, **kwargs):
     if not sess_key:
         sess_key = kwargs.get('panda').get('sess_key')
 
-    proxies = None
+    proxy = None
     if 'proxy' in kwargs.get('panda'):
-        proxies = {
-            'http': kwargs.get('panda').get('proxy'),
-            'https': kwargs.get('panda').get('proxy')
-        }
+        proxy = kwargs.get('panda').get('proxy')
 
     try:
         res = httpx.post('https://api.pandalive.co.kr/v1/live/play', data={
@@ -28,7 +25,7 @@ def get_stream(room_id, **kwargs):
             'referer': 'https://www.pandalive.co.kr/',
             'user-agent': random.choice(USER_AGENTS),
             'x-device-info': '{"t":"webPc","v":"1.0","ui":17784756}'
-        }, proxies=proxies).json()
+        }, proxy=proxy).json()
     except (httpx.HTTPError, ValueError):
         return False
 
